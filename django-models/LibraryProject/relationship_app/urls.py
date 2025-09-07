13209434d9f77b04c88d53c1767a87779023f8ac
Dev-MiMi <miracleolagundoye1@gmail.com>
2025-09-07 15:27:22 +0100

    yyy

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -3,4 +3,5 @@ from .views import list_books
 
 urlpatterns = [
         path('books/', list_books, name='list_books'),
+        path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -8,3 +8,8 @@ def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
+class LibraryDetails(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'  # This ensures your template receives 'library'
+
