84a114d68fd19a3c1df76f5f506a40e5216d47e8
Dev-MiMi <miracleolagundoye1@gmail.com>
2025-09-07 12:16:35 +0100

    yvgfg

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,5 +5,5 @@ from .models import Book
 
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
