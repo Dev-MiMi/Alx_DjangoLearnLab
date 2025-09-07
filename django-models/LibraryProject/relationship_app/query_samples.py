3a405609b8913dc9417794f7be16123cd58cdd92
Dev-MiMi <miracleolagundoye1@gmail.com>
2025-09-07 09:47:25 +0100

    tvig

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,29 +1,28 @@
+from relationship_app.models import Author, Book, Library, Librarian
+
 # Authors
 achebe = Author.objects.create(name="Chinua Achebe")
 orwell = Author.objects.create(name="George Orwell")
 
 # Books
-book1 = Book.objects.create(name="Things Fall Apart", author=achebe)
-book2 = Book.objects.create(name="Arrow of God", author=achebe)
-book3 = Book.objects.create(name="1984", author=orwell)
+book1 = Book.objects.create(title="Things Fall Apart", author=achebe)
+book2 = Book.objects.create(title="Arrow of God", author=achebe)
+book3 = Book.objects.create(title="1984", author=orwell)
 
 # Libraries
 lib1 = Library.objects.create(name="City Library")
-lib2 = Library.objects.create(name="Central Library")
 
 lib1.books.add(book1, book2, book3)
-lib2.books.add(book1, book2)
 
 # Librarians (attach to libraries with OneToOneField)
 librarian1 = Librarian.objects.create(name="Mary Johnson", library=lib1)
-librarian2 = Librarian.objects.create(name="Samuel John", library=lib2)
 
 # List all books in a library
 library_name = "City Library"
 lib = Library.objects.get(name=library_name) 
 
 for book in lib.books.all():
-    print(book.name)
+    print(book.title)
 
 
 # Query all books by a specific author
@@ -32,7 +31,7 @@ author = Author.objects.get(name=author_name)
 
 books = Book.objects.filter(author=author)
 for book in books:
-    print(book.name)
+    print(book.title)
 
 # Retrieve the librarian for a library
 library_name = "City Library"
