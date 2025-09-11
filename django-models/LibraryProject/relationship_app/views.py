eda89c534f3b6f3d31ab87c8888db2a11b4e6d56
Dev-MiMi <miracleolagundoye1@gmail.com>
2025-09-11 11:49:02 +0100

    hbii

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,7 @@ from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
 from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationFor
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views 
 
