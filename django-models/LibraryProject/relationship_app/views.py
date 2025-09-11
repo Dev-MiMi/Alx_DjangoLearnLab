322d1da266efb1ab5de78ad886c3e6058f208df8
Dev-MiMi <miracleolagundoye1@gmail.com>
2025-09-11 16:22:23 +0100

    %^£¥

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -16,3 +16,17 @@ class LibraryDetails(DetailView):
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'  # This ensures your template receives 'library'
 
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f"Account created for {username}!")
+            return redirect('login')  # Redirect to login page after registration
+        else:
+            messages.error(request, "Error in registration. Please check your input.")
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
+
