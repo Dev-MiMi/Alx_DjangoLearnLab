59fc34e189f35fedee26fcb92a7474d29dc0919c
Dev-MiMi <miracleolagundoye1@gmail.com>
2025-09-14 20:04:29 +0100

    fhue

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -3,6 +3,8 @@
 # Register your models here.
 from django.contrib import admin
 from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ['title', 'author', 'publication_year']
@@ -10,3 +12,43 @@ class BookAdmin(admin.ModelAdmin):
     search_fields = ['author', 'title',]
 
 admin.site.register(Book, BookAdmin)
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+
+    # Columns visible in the user list page
+    list_display = ("username", "email", "date_of_birth", "is_staff", "is_active")
+    list_filter = ("is_staff", "is_active", "date_of_birth")
+
+    # Fields visible when editing an existing user
+    fieldsets = (
+        (None, {"fields": ("username", "email", "password")}),
+        ("Personal Info", {"fields": ("first_name", "last_name", "date_of_birth", "profile_photo")}),
+        ("Permissions", {"fields": ("is_staff", "is_active", "is_superuser", "groups", "user_permissions")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+
+    # Fields visible when creating a new user in admin
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "username",
+                "email",
+                "date_of_birth",
+                "profile_photo",
+                "password1",
+                "password2",
+                "is_staff",
+                "is_active"
+            )}
+        ),
+    )
+
+    search_fields = ("email", "username")
+    ordering = ("email",)
+
+
+# Register the custom admin
+admin.site.register(CustomUser, CustomUserAdmin)
