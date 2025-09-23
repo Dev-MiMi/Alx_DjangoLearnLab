aa37d35e6805e5a3d6ac9f57b54d8e555b8a7dc5
Dev-MiMi <miracleolagundoye1@gmail.com>
2025-09-23 13:20:49 +0100

    tghu

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
-
-# Create your views here.
-
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 @api_view(['GET'])
 def book_list(request):
     books = Book.objects.all()
     serializer = BookSerializer(books, many=True)
     return Response(serializer.data)
+
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
