9563630dfc11b36dfa6b88040c84105461fd299b
Dev-MiMi <miracleolagundoye1@gmail.com>
2025-09-28 10:13:27 +0100

    tft

--- advanced_api_project/advanced_api_project/settings.py
+++ advanced_api_project/advanced_api_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-pmuj*%8j*l$pm@19j385bjn$x)^9qglwdd)kn+xb@rkm$6z(mq"
+SECRET_KEY = "django-insecure-1&6ucs3iz(^rke(asugous#vg2il0j)-k64+r=l+5g5vnybd^y"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
