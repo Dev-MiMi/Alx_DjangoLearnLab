5abc27c7fce7a42e35c28289d4c65df4b3203520
Dev-MiMi <miracleolagundoye1@gmail.com>
2025-10-09 22:08:13 +0100

    registerview

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,20 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,
+redirect
+from django.views import view
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationFormfrom django.contrib import messages
+from django.contrib.auth.models import User
+from django.utils.decorators import metho
+d_decorator
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 def home_view(request):
     return render(request, 'blog/home.html')
-
-# blog/views.py
-from django.urls import reverse_lazy
-from django.views import View
-from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib import messagesi
-from .forms import CustomUserCreationForm
 
 class RegisterView(View):
     template_name = 'blog/register.html'
@@ -31,3 +29,26 @@ class RegisterView(View):
             return redirect('login')
         return render(request, self.template_name, {'form': form})
 
+@method_decorator(login_required, name='dispatch')
+class ProfileView(View):
+    template_name = 'blog/profile.html'
+
+    def get(self, request):
+        """Display user profile details"""
+        return render(request, self.template_name, {'user': request.user})
+
+    def post(self, request):
+        """Handle profile updates"""
+        user = request.user
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+
+        # Update the fields if they were provided
+        if username:
+            user.username = username
+        if email:
+            user.email = email
+
+        user.save()
+        messages.success(request, 'Profile updated successfully!')
+        return redirect('profile')
