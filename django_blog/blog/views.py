2116a0402b316a19cf08a00d86e076e8a0573f9b
Dev-MiMi <miracleolagundoye1@gmail.com>
2025-10-09 15:00:10 +0100

    registerview

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from .forms import CustomUserCreationForm
 
 # Create your views here.
+
+def home_view(request):
+    return render(request, 'blog/home.html')
+
+class RegisterView(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = "blog/register.html"
+    success_url = reverse_lazy("login")
+
