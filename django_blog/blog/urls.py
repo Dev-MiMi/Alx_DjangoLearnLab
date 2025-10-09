0815c38e5e3a3825a21439053e5266580fa9dbe2
Dev-MiMi <miracleolagundoye1@gmail.com>
2025-10-09 15:05:35 +0100

    registerview

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,9 +1,10 @@
-from .views import RegisterView, home_view
+from .views import RegisterView, home_view, ProfileView
 from django.urls import path
 from django.contrib.auth import views as auth_view
 
 urlpatterns = [
         path("", home_view, name="home"),
+        path("profile/", ProfileView.as_view, name="profile")
         path("register/", RegisterView.as_view(), name="signup"),
         path("login/", auth_view.LoginView.as_view(), name="login"),
         path("logout/", auth_view.LogoutView.as_view(), name="logout"),
