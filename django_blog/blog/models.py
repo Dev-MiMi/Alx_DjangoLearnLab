d0d5f66e719d2295144d4e4ff3b9f52af710dce1
Dev-MiMi <miracleolagundoye1@gmail.com>
2025-10-10 09:56:51 +0100

    comments, search

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from taggit.managers import TaggableManager  # ✅ import Taggit manager
 
 # Create your models here.
 class Tag(models.Model):
@@ -17,7 +18,8 @@ class Post(models.Model):
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="authors")
-    tags = models.ManyToManyField(Tag, blank=True, related_name="posts")  # 🔥 Added
+    tags = models.ManyToManyField(Tag, blank=True, related_name="posts")
+    tags = TaggableManager()
 
     def __str__(self):
         return self.title
